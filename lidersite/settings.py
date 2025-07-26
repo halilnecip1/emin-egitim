e0aa9d88c8317712e89788955a954a66c0421a09
muslumanoldum <halilnecip25@outlook.com>
2025-07-26 23:12:17 +0300

    PostgreSQL ve dj-database-url ayarları eklendi

--- lidersite/settings.py
+++ lidersite/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
 
 DATABASE_URL = os.environ.get('DATABASE_URL')
 if DATABASE_URL:
-    DATABASES['default'] = dj_database_url.config(default=DATABASE_URL, conn_max_age=600)
+    DATABASES['default'] = dj_database_url.config(default=DATABASE_URL, conn_max_age=600, ssl_require=True)
 
 
 AUTH_PASSWORD_VALIDATORS = [
