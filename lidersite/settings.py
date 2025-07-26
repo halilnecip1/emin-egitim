9707133eccbe732a414cdc7f4808fb75344efbe8
muslumanoldum <halilnecip25@outlook.com>
2025-07-27 00:11:46 +0300

    STATICFILES_DIRS eklendi ve urls.py kontrol edildi

--- lidersite/settings.py
+++ lidersite/settings.py
@@ -114,6 +114,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [ # <-- BU SATIRI EKLEYİN
+    BASE_DIR / 'static', # <-- VE BU SATIRI EKLEYİN
+]
+
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
